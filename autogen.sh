452f66400a9517147e48d489b44894b2ed645ee6
spl <simon@raspberrypi.org>
2016-08-08 17:09:39 +0100

    More package tidying.

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="lightdm-gtk-greeter"
+PKG_NAME="pi-greeter"
 REQUIRED_AUTOMAKE_VERSION=1.7
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level lightdm-gtk-greeter directory"
+    echo " top-level pi-greeter directory"
     exit 1
 }
 
